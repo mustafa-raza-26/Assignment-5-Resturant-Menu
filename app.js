608d40966938921008ced29781c2ed185c995144
Mustafa_Raza <mustafa778823@gmail.com>
2025-07-01 11:43:37 +0500

    Resturant Menu

---
+++ app.js
@@ -0,0 +1,117 @@
+alert('Welcome To Our Resturant')
+let userChoise = +prompt(' 1 Biryani \n 2 Pizza \n 3 Burger \n 4 BBQ')
+
+// Biryani
+
+if (userChoise == 1) {
+    console.log('You Select The Biryani');
+    let biryaniType = +prompt(' Now Select the Biryani \n 1 Beaf Biryani \n 2 Meaton Biryani \n 3 Chicken Biryani \n 4 Chola Biryani')
+
+if (biryaniType == 1) {
+    console.log('You Select Beaf Biryani');
+    alert('You Select Beaf Biryani')
+}else if (biryaniType == 2) {
+    console.log('You Select Meaton Biryani');
+    alert('You Select Meaton Biryani')
+}else if (biryaniType == 3) {
+    console.log('You Select Chicken Biryani');
+    alert('You Select Chicken Biryani')
+}else if (biryaniType == 4) {
+    console.log('You Select Chola Biryani');
+    alert('You Select Chola Biryani')
+}
+else{
+    console.log('Enter the Number between 1 to 4');
+    alert('Enter the Number between 1 to 4')
+}
+}
+// else{
+//     console.log('Enter Number 1 for Biryani');
+// }
+
+
+// Pizza
+
+if (userChoise == 2) {
+    console.log('You Select The Pizza');
+    let pizzaType = +prompt(' Now Select the Pizza Flavour \n 1 Chicken Fajeta \n 2 Chicken Tikka \n 3  \n 4 ')
+
+if (pizzaType == 1) {
+    console.log('You Select Chicken Fajeta Flavor');
+    alert('You Select Chicken Fajeta Flavor')
+}else if (pizzaType == 2) {
+    console.log('You Select Chicken TikkaFlavor');
+    alert('You Select Chicken TikkaFlavor')
+}else if (pizzaType == 3) {
+    console.log('You Select Flavor');
+    alert('You Select Flavor')
+}else if (pizzaType == 4) {
+    console.log('You Select Flavor');
+    alert('You Select Flavor')
+}
+else{
+    console.log('Enter the Number between 1 to 4');
+    alert('Enter the Number between 1 to 4')
+}
+}
+// else{
+//     console.log('Enter Number 2 for Pizza');
+// }
+
+
+// Burger
+
+if (userChoise == 3) {
+    console.log('You Select The Burger');
+    let burgerType = +prompt(' Now Select the Burger Type \n 1 Zinger Burger \n 2 Beaf Burger \n 3 Double Pati Burger  \n 4 Egg Mayo Buger')
+
+if (burgerType == 1) {
+    console.log('You Select Zinger Burger');
+    alert('You Select Zinger Burger')
+}else if (burgerType == 2) {
+    console.log('You Select Beaf Burger');
+    alert('You Select Beaf Burger')
+}else if (burgerType == 3) {
+    console.log('You Select Double Pati Burger');
+    alert('You Select Double Pati Burger')
+}else if (burgerType == 4) {
+    console.log('You Select Egg Mayo Buger');
+    alert('You Select Egg Mayo Buger')
+}
+else{
+    console.log('Enter the Number between 1 to 4');
+    alert('Enter the Number between 1 to 4')
+}
+}
+// else{
+//     console.log('Enter Number 3 for Burger');
+// }
+
+
+// BBQ
+
+if (userChoise == 4) {
+    console.log('You Select The BBQ');
+    let bbqType = +prompt(' Now Select the BBQ Type \n 1 Malai Boti \n 2 Beaf Bihari Boti \n 3 Chicken Tikka \n 4 Kabab')
+
+if (bbqType == 1) {
+    console.log('You Select Malai Boti');
+    alert('You Select Malai Boti')
+}else if (bbqType == 2) {
+    console.log('You Select Beaf Bihari Boti');
+    alert('You Select Beaf Bihari Boti')
+}else if (bbqType == 3) {
+    console.log('You Select Chicken Tikka');
+    alert('You Select Chicken Tikka')
+}else if (bbqType == 4) {
+    console.log('You Select Kabab');
+    alert('You Select Kabab')
+}
+else{
+    console.log('Enter the Number between 1 to 4');
+    alert('Enter the Number between 1 to 4')
+}
+}
+// else{
+//     console.log('Enter Number 3 for Burger');
+// }
